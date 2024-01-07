f03ea95dcf3fc7d953736226388effc1fc85387d
Mariya Spiridonova <kolibri0509@gmail.com>
2024-01-07 21:48:06 +0300

    step2

--- index.js
+++ index.js
@@ -29,6 +29,10 @@ app.get('/', (request, response) => {
 app.get('/api/persons',(request,response)=> {
     response.json(persons)
 })
+app.get('/info', (request, response)=> {
+    response.send(`<p>Phonebook has info for ${persons.length} people</p>
+    <p>${Date()}</p>`)
+})
 
 const PORT = 3001
 app.listen(PORT)
