4b24bf12dfe2a2c31ff859a2e934b3b522051061
Mariya Spiridonova <kolibri0509@gmail.com>
2024-01-22 12:33:57 +0300

    step2

--- index.js
+++ index.js
@@ -3,36 +3,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 const app = express()
 require('dotenv').config()
-
-const mongoose = require('mongoose');
-mongoose.set('strictQuery', false)
-
-const url = process.env.MONGODB_URI
-
-console.log('connecting to', url)
-
-mongoose.connect(url)
-  .then(result => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
-
-const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
-})
-
-const Person = mongoose.model('Person', personSchema)
-
-personSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject.__v
-  }
-})
+const Person = require('./models/person')
 
 app.use(express.json())
 app.use(cors())
@@ -41,8 +12,7 @@ app.use(express.static('dist'))
 morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons = [
-]
+let persons = []
 
 app.get('/api/persons',(request,response)=> {
     Person.find({})
@@ -67,9 +37,6 @@ app.delete('/api/persons/:id', (request, response)=> {
   persons = persons.filter(p => p.id !== id)
   response.status(204).end()
 })
-const randomId = (max) =>{
-  return Math.floor(Math.random()*max)
-}
 app.post('/api/persons', (request, response) => {
   const body = request.body
 
@@ -83,14 +50,11 @@ app.post('/api/persons', (request, response) => {
   if(persons.find(p => p.number === body.number))
    return response.status(400).json({error:'number must be unique'})
      
-  const person = {
+  const person = new Person({
     name: body.name,
     number: body.number,
-    id: randomId(10000)
-  }
-  console.log(person)
-  persons.concat(person)
-  response.json(person)
+  })
+  person.save().then(savedPerson => response.json(savedPerson))
 })
 
 const PORT = process.env.PORT 
