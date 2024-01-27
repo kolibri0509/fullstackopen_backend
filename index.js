bf365f2e2ecb589e1126b6e40a4235eb1ff03eca
Mariya Spiridonova <kolibri0509@gmail.com>
2024-01-27 16:41:06 +0300

    step5

--- index.js
+++ index.js
@@ -19,43 +19,54 @@ app.get('/api/persons',(request,response)=> {
     .then(persons => response.json(persons))
     .catch(error => next(error))
 })
-app.get('/info', (request, response)=> {
-    response.send(`<p>Phonebook has info for ${persons.length} people</p>
-    <p>${Date()}</p>`)
-})
-app.get('/api/persons/:id', (request, response)=>{
-  const id = Number(request.params.id)
-  const person = persons.find(p=>p.id === id)
-  if(person){
-    response.json(person)
-  }else{
-    response.statusMessage = 'The requested person does not exist.'
-    response.status(404).end()
-  }
-})
+// app.get('/info', (request, response, next)=> {
+//     const count = mongoose.Collection.countDocuments({})
+//     response.send(`<p>Phonebook has info for ${count} people</p>
+//     <p>${Date()}</p>`).catch(error => next(error)) 
+// })
+// app.get('/api/persons/:id', (request, response, next)=>{
+//   Person.findById(request.params.id).then(person => {
+//     if(person){
+//       response.json(person)
+//     }else{
+//       response.status(404).end()
+//     }  
+//   })
+//   .catch( error => next(error))
+// })
+
 app.delete('/api/persons/:id', (request, response)=> {
   const id = request.params.id
   Person.findByIdAndDelete(id).then(result => response.status(204).end())
   .catch(error => next(error))
 })
+
 app.post('/api/persons', (request, response) => {
   const body = request.body
 
   if(!body.name || !body.number){
     response.statusMessage = 'content missing'
     return response.status(400).end()
-  } 
-  if(persons.find(p => p.name === body.name)) 
-  return response.status(400).json({error:'name must be unique'})
-
-  if(persons.find(p => p.number === body.number))
-   return response.status(400).json({error:'number must be unique'})
-     
+  }     
   const person = new Person({
     name: body.name,
     number: body.number,
   })
   person.save().then(savedPerson => response.json(savedPerson))
+})
+
+app.put('/api/persons/:id',(request, response, next) => {
+  const body = request.body
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  }
+  Person.findByIdAndUpdate(request.params.id, person, {new: true})
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
 
 const errorHandler = (error, request, response, next) => {
